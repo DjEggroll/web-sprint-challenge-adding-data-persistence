8a157ab2cea5a42fc5511e053cfc5a6e66f599fc
Jiahui Feng <feng_jeffrey@yahoo.com>
2022-08-02 20:33:40 -0400

    complete

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,37 @@
 // build your `/api/projects` router here
+const express = require('express');
+const helpers = require('./model');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        let projects = await helpers.getProjects();
+        projects.map(project => {
+            if(project.project_completed === 0){
+                project.project_completed = false
+            } else {
+                project.project_completed = true
+            }
+        })
+        res.status(200).json(projects)
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        let newProject = await helpers.postProject(req.body);
+            if(newProject.project_completed === 0){
+                newProject.project_completed = false;
+            } else {
+                newProject.project_completed = true;
+            }
+        res.status(201).json(newProject);
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,37 @@
 // build your `/api/tasks` router here
+const express = require('express');
+const helpers = require('./model');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        let tasks = await helpers.getTasks();
+        tasks.map(task => {
+            if(task.task_completed === 0){
+                task.task_completed = false
+            } else {
+                task.task_completed = true
+            }
+        })
+        res.status(200).json(tasks)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        let newTask = await helpers.postTask(req.body);
+            if(newTask.task_completed === 0){
+                newTask.task_completed = false
+            } else {
+                newTask.task_completed = true
+            }
+        res.status(201).json(newTask);
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router;
